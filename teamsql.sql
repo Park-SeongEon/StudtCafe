f8041216437537c5c43a4b7ce2a8ac19f468b23b
jiwan <audwldhks0@gmail.com>
2023-06-13 16:47:34 +0900

    SQL 변경

--- teamsql.sql
+++ teamsql.sql
@@ -1,15 +1,13 @@
 -- --------------------------------------------------------
 -- 호스트:                          127.0.0.1
--- 서버 버전:                        10.10.4-MariaDB - mariadb.org binary distribution
+-- 서버 버전:                        10.11.2-MariaDB - mariadb.org binary distribution
 -- 서버 OS:                        Win64
--- HeidiSQL 버전:                  12.3.0.6589
+-- HeidiSQL 버전:                  11.3.0.6295
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
@@ -24,8 +22,8 @@ CREATE TABLE IF NOT EXISTS `board` (
   `brd_no` int(10) NOT NULL AUTO_INCREMENT,
   `title` varchar(500) NOT NULL,
   `content` varchar(4000) DEFAULT NULL,
-  `FILE_NAME` varchar(100) DEFAULT NULL,
-  `REG_DATE` date NOT NULL DEFAULT current_timestamp(),
+  `file_name` varchar(100) DEFAULT NULL,
+  `regDate` date NOT NULL DEFAULT current_timestamp(),
   `kate_no` int(10) NOT NULL,
   `vote_no` int(5) NOT NULL DEFAULT 0,
   `cnt` int(5) DEFAULT NULL,
@@ -33,6 +31,8 @@ CREATE TABLE IF NOT EXISTS `board` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- 테이블 데이터 studycafe.board:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `board` DISABLE KEYS */;
+/*!40000 ALTER TABLE `board` ENABLE KEYS */;
 
 -- 테이블 studycafe.board_comment 구조 내보내기
 CREATE TABLE IF NOT EXISTS `board_comment` (
@@ -45,20 +45,24 @@ CREATE TABLE IF NOT EXISTS `board_comment` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- 테이블 데이터 studycafe.board_comment:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `board_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `board_comment` ENABLE KEYS */;
 
 -- 테이블 studycafe.kategorie 구조 내보내기
 CREATE TABLE IF NOT EXISTS `kategorie` (
   `kate_no` int(10) NOT NULL AUTO_INCREMENT,
   `kate_name` varchar(10) NOT NULL,
-  `user_type_cd` varchar(2) NOT NULL,
+  `user_type_cd` varchar(2) NOT NULL DEFAULT '02',
   PRIMARY KEY (`kate_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- 테이블 데이터 studycafe.kategorie:~2 rows (대략적) 내보내기
+-- 테이블 데이터 studycafe.kategorie:~3 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `kategorie` DISABLE KEYS */;
 INSERT INTO `kategorie` (`kate_no`, `kate_name`, `user_type_cd`) VALUES
-	(1, '게시판', ''),
-	(2, '공지사항', ''),
-	(3, '유저목록', '');
+	(1, '공지사항', '01'),
+	(2, '유저목록', '01'),
+	(3, '게시판', '02');
+/*!40000 ALTER TABLE `kategorie` ENABLE KEYS */;
 
 -- 테이블 studycafe.user 구조 내보내기
 CREATE TABLE IF NOT EXISTS `user` (
@@ -77,8 +81,10 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- 테이블 데이터 studycafe.user:~1 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`user_id`, `user_pwd`, `user_name`, `user_email`, `user_cp`, `user_addr`, `user_daddr`, `regData`, `joinData`, `user_grade`, `user_type_cd`) VALUES
 	('admin', '12345', '관리자', '관리자', '관리자', '관리자', '관리자', '2023-06-13', NULL, 'E', '01');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 -- 테이블 studycafe.user_type_cd 구조 내보내기
 CREATE TABLE IF NOT EXISTS `user_type_cd` (
@@ -87,11 +93,12 @@ CREATE TABLE IF NOT EXISTS `user_type_cd` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- 테이블 데이터 studycafe.user_type_cd:~2 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `user_type_cd` DISABLE KEYS */;
 INSERT INTO `user_type_cd` (`user_type_cd`, `USER_TYPE_NAME`) VALUES
 	('01', '관리자'),
 	('02', '일반');
+/*!40000 ALTER TABLE `user_type_cd` ENABLE KEYS */;
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
